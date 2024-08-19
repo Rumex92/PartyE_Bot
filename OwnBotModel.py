f94a90a1acee8a462571b06998aa66610f152168
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-08-20 01:58:47 +0630

    kaggle model

---
+++ OwnBotModel.py
@@ -0,0 +1,20 @@
+import joblib
+
+loaded_pipe = joblib.load('pipeline_model.pkl', mmap_mode='r')
+
+def get_response(question):
+    response = loaded_pipe.predict([question])[0]
+    return response
+
+# Function to interact with the user
+def chat():
+    while True:
+        question = input("You: ")
+        if question.lower() == 'quit':
+            print("Chatbot: Goodbye!")
+            break
+        response = get_response(question)
+        print("Chatbot:", response)
+
+# Start the chat
+chat()
